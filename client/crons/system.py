ed57956a9d84791dc296e2c1f2ae1729cdbad364
frznfngrs <19555532+frznfngrs@users.noreply.github.com>
2023-12-29 11:09:46 -0600

    Update system.py

--- client/crons/system.py
+++ client/crons/system.py
@@ -6,38 +6,38 @@ import GPUtil
 from lib.decorators import set_interval
 import settings
 
+
 def run():
     db = Database()
     _time = int(time.time())
-
     cpu_usage = psutil.cpu_percent()
     disk_space = psutil.disk_usage('/var/lib/docker')
-    disk_space_root = psutil.disk_usage('/')
     disk_usage = psutil.disk_io_counters(perdisk=False)
+    #    network_usage = psutil.net_io_counters(pernic=True)["docker0"]
     network_usage = psutil.net_io_counters(pernic=True)
+    #    cputemp = 0 if open("/sys/class/thermal/thermal_zone0/temp",'r').readlines() == None else open("/sys/class/thermal/thermal_zone0/temp",'r').readlines()
+    #    temperatures2 = float(cputemp[0])/1000
     networksum = 0
 
     def get_average(items):
         items = [item.current for item in items]
         return sum(items) / len(items)
 
-    def get_CPUTemps():
-        testCPUTemp = psutil.sensors_temperatures()
-        if 'coretemp' in testCPUTemp:
-            testCPUTemp = get_average(testCPUTemp['coretemp'])
-            return testCPUTemp
-        if 'k10temp' in testCPUTemp:
-            testCPUTemp = (testCPUTemp.get('k10temp')[0][1])
-            return testCPUTemp
-        testCPUTemp = open("/sys/class/thermal/thermal_zone0/temp", 'r').readlines()
-        if testCPUTemp:
-            testCPUTemp = float(testCPUTemp[0]) / 1000
-            return testCPUTemp
-        return 0
+#    def get_CPUTemps():
+#        testCPUTemp = psutil.sensors_temperatures()
+#        if 'coretemp' in testCPUTemp :
+#            testCPUTemp = get_average(testCPUTemp['coretemp'])
+#            return testCPUTemp
+#        if 'k10temp' in testCPUTemp :
+#            testCPUTemp = (testCPUTemp.get('k10temp')[0][1])
+#            return testCPUTemp
+#        testCPUTemp = open("/sys/class/thermal/thermal_zone0/temp", 'r').readlines()
+#        if testCPUTemp != None:
+#            testCPUTemp = float(testCPUTemp[0]) / 1000
+#           return testCPUTemp
+#        return 0
 
-    hardware_records = []
-
-    hardware_records.append({
+    db.insert_hardware(_time, {
         "component": "ram",
         "hw_id": None,
         "utilisation": psutil.virtual_memory().percent,
@@ -45,7 +45,7 @@ def run():
         "power_consumption": None,
     })
 
-    hardware_records.append({
+    db.insert_hardware(_time, {
         "component": "disk_space",
         "hw_id": None,
         "utilisation": disk_space.percent,
@@ -53,48 +53,55 @@ def run():
         "power_consumption": None,
     })
 
-        hardware_records.append({
-        "component": "disk_space_root",
+    '''
+    db.insert_hardware(_time, {
+        "component": "disk_usage",
         "hw_id": None,
-        "utilisation": disk_space.percent,
+        "utilisation": disk_space.read_count,
         "temperature": None,
         "power_consumption": None,
     })
-
-    # Add other hardware records in a similar way
-
+    '''
+    #    for item in network_usage:
+    #        db.insert_hardware(_time, {
+    #            "component": "network",
+    #            "hw_id": none,
+    #            "utilisation": network_usage[item].bytes_recv + network_usage[item].bytes_sent,
+    #            "temperature": None,
+    #            "power_consumption": None,
+    #        })
     for item in network_usage:
         networksum = networksum + network_usage[item].bytes_recv + network_usage[item].bytes_sent
 
-    hardware_records.append({
+    db.insert_hardware(_time, {
         "component": "network",
         "hw_id": None,
         "utilisation": networksum,
         "temperature": None,
         "power_consumption": None,
     })
+    #    db.insert_hardware(_time, {
+    #        "component": "network",
+    #        "hw_id": None,
+    #        "utilisation": network_usage.bytes_recv + network_usage.bytes_sent,
+    #        "temperature": None,
+    #        "power_consumption": None,
+    #    })
 
-    hardware_records.append({
+    db.insert_hardware(_time, {
         "component": "cpu",
         "hw_id": None,
         "utilisation": psutil.cpu_percent(),
         "temperature": None,
+        #        "temperature": temperatures,
         "power_consumption": None,
     })
 
     for gpu in GPUtil.getGPUs():
-        gpu_power_consumption = float(exec("gpu_power", {"gpu_id": gpu.id}))
-        hardware_records.append({
+        db.insert_hardware(_time, {
             "component": "gpu",
             "hw_id": gpu.id,
             "utilisation": gpu.load * 100,
             "temperature": gpu.temperature,
-            "power_consumption": gpu_power_consumption,  # Use the obtained power consumption value
+            "power_consumption": float(exec("gpu_power", args=(gpu.id))),
         })
-
-
-    # Insert data in batches
-    db.insert_batch_hardware(hardware_records, _time)
-
-if __name__ == "__main__":
-    run()
